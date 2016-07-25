7854b2725459eeb8ff40ceedf82716c1d89578d6
zhangyaobin <zhangyaobin@ttyongche.com>
2016-07-25 17:07:41 +0800

    commit all

---
+++ app/src/main/java/com/zhangyaobin/infiniteloopviewpager/MainActivity.java
@@ -0,0 +1,182 @@
+package com.zhangyaobin.infiniteloopviewpager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v4.view.ViewPager;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements ViewPager.OnPageChangeListener {
+    /**
+     * MyViewPager定制性较强，可以放在scrollview中，也可放在listview的头部或底部
+     */
+    MyViewPager mMyViewPager;
+    LinearLayout mDotLayout;
+    List<String> mAdImageList = new ArrayList<>();
+    CountdownTimer mCountdownTimer;
+    private int currentItem = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMyViewPager = (MyViewPager) findViewById(R.id.MyViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.DotLayout);
+        mCountdownTimer = new CountdownTimer(3000, 1000);
+        initData();
+        currentItem = mAdImageList.size() < 2 ? 1
+                : mAdImageList.size() * 10;
+        createDot(currentItem);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this, mAdImageList, new ViewPagerAdapter.OnBannerClickListener() {
+            @Override
+            public void onBannerClick(String action) {
+                Toast.makeText(MainActivity.this,"点击"+action,Toast.LENGTH_LONG).show();
+            }
+        });
+        // 速度匀速，默认为加速
+        LinearInterpolator interpolator = new LinearInterpolator();
+        ViewPagerScroller viewPagerScroller = new ViewPagerScroller(this, interpolator);
+        viewPagerScroller.initViewPagerScroll(mMyViewPager);
+
+
+        mMyViewPager.setAdapter(viewPagerAdapter);
+
+        mMyViewPager.setCurrentItem(currentItem, true);
+        mMyViewPager.setOnPageChangeListener(this);
+
+    }
+
+    private void initData() {
+        mAdImageList.add("http://img5.imgtn.bdimg.com/it/u=3425851328,2681317699&fm=206&gp=0.jpg");
+        mAdImageList.add("http://pic9.nipic.com/20100904/4845745_195609329636_2.jpg");
+        mAdImageList.add("http://img1.3lian.com/2015/w7/90/d/5.jpg");
+        mAdImageList.add("http://pic1.nipic.com/2008-12-09/200812910493588_2.jpg");
+        mAdImageList.add("http://img1.3lian.com/2015/w7/90/d/1.jpg");
+        mAdImageList.add("http://pic1.nipic.com/2008-12-25/2008122510134038_2.jpg");
+        mAdImageList.add("http://pic10.nipic.com/20101103/5063545_000227976000_2.jpg");
+    }
+
+    // 创建顶部广告对应的小圆点
+    protected void createDot(int currentIndex) {
+        mDotLayout.removeAllViews();
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(5, 0, 5, 0);
+        // Log.d("zhangyaobin", "list.size=" + list.size());
+        if (mAdImageList != null && mAdImageList.size() > 1) {
+            for (int i = 0; i < mAdImageList.size(); i++) {
+                ImageView image = new ImageView(this);
+                image.setLayoutParams(lp);
+                image.setScaleType(ImageView.ScaleType.MATRIX);
+                image.setImageResource(R.mipmap.banner_point_empty);
+                mDotLayout.addView(image);
+            }
+            ((ImageView) mDotLayout.getChildAt(currentIndex % mAdImageList.size()))
+                    .setImageResource(R.mipmap.banner_point_fill);
+        }
+    }
+
+    /**
+     * 设置选中的tip的背景
+     *
+     * @param selectItemsIndex
+     */
+    private void setDotBackground(int selectItemsIndex) {
+        for (int i = 0; i < mDotLayout.getChildCount(); i++) {
+            if (i == selectItemsIndex) {
+                ((ImageView) mDotLayout.getChildAt(i))
+                        .setImageResource(R.mipmap.banner_point_fill);
+            } else {
+                ((ImageView) mDotLayout.getChildAt(i))
+                        .setImageResource(R.mipmap.banner_point_empty);
+            }
+        }
+    }
+
+
+    @Override
+    public void onPageScrolled(int i, float v, int i1) {
+
+    }
+
+    @Override
+    public void onPageSelected(int i) {
+        setDotBackground(i % mAdImageList.size());
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+
+    }
+
+    public class CountdownTimer extends CountDownTimer {
+        public CountdownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onFinish() {
+            currentItem++;
+            mMyViewPager.setCurrentItem(currentItem);
+            start();
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+    }
+
+    private boolean isLooping = false;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLoop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLoop();
+    }
+
+    private void startLoop() {
+        if (mCountdownTimer != null && !isLooping) {
+            isLooping = true;
+            mCountdownTimer.start();
+        }
+    }
+
+    private void stopLoop() {
+        isLooping = false;
+        mCountdownTimer.cancel();
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+
+        mCountdownTimer = null;
+        super.onDestroy();
+    }
+
+//    OnBannerClickListener mOnBannerClickListener;
+//
+//    public interface OnBannerClickListener {
+//        void onBannerClick(String action);
+//    }
+//
+//    public void setOnBannerClickListener(OnBannerClickListener onBannerClickListener) {
+//        mOnBannerClickListener = onBannerClickListener;
+//    }
+}
---
+++ app/src/main/java/com/zhangyaobin/infiniteloopviewpager/ViewPagerAdapter.java
@@ -0,0 +1,144 @@
+package com.zhangyaobin.infiniteloopviewpager;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by zhangyaobin on 16/7/25.
+ */
+public class ViewPagerAdapter extends PagerAdapter {
+    // int mCount = mAdImageList.size();
+    List<String> mAdImageList;
+    Context mContext;
+
+    public ViewPagerAdapter(Context context, List<String> mAdImageList, OnBannerClickListener onBannerClickListener) {
+        this.mContext = context;
+        this.mAdImageList = mAdImageList;
+        this.mOnBannerClickListener=onBannerClickListener;
+        initImageView();
+    }
+
+    public void updateData(List<String> mAdImageList) {
+        this.mAdImageList = mAdImageList;
+    }
+
+    @Override
+    public int getCount() {
+        if (mAdImageList.size() <= 1) {
+            return 1;
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    /**
+     * 判断出去的view是否等于进来的view 如果为true直接复用
+     */
+    @Override
+    public boolean isViewFromObject(View arg0, Object arg1) {
+        return arg0 == arg1;
+    }
+
+    /**
+     * 销毁预加载以外的view对象, 会把需要销毁的对象的索引位置传进来就是position
+     */
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        if (mAdImageList.size() == 0) {
+            container
+                    .removeView(mImageViews[position / 1 % 2][0]);
+        } else {
+            container.removeView(mImageViews[position
+                    / mAdImageList.size() % 2][position
+                    % mAdImageList.size()]);
+        }
+    }
+
+    /**
+     * 创建一个view
+     */
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+        ImageView view = null;
+        if (mAdImageList.size() == 0) {
+            view = mImageViews[position / 1 % 2][0];
+        } else {
+            view = mImageViews[position / mAdImageList.size() % 2][position
+                    % mAdImageList.size()];
+        }
+        container.addView(view, 0);
+        view.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+//                 Toast.makeText(mContext, "点击事件", Toast.LENGTH_LONG).show();
+                Log.d("zhangyaobin","mOnBannerClickListener="+mOnBannerClickListener);
+                if (mOnBannerClickListener != null) {
+//                        跳转至对应的界面
+
+                    if (mAdImageList.size() == 0) {
+                        mOnBannerClickListener.onBannerClick(""+position);
+                    } else {
+                        mOnBannerClickListener.onBannerClick(""+position
+                                % mAdImageList.size());
+                    }
+                }
+            }
+        });
+        return view;
+    }
+
+    /**
+     * 装ImageView数组
+     */
+    protected ImageView[][] mImageViews;
+
+    // 初始化imageview
+    protected void initImageView() {
+        ImageView iv;
+        // imageViewList.clear();
+        mImageViews = new ImageView[2][];
+        if (mAdImageList != null && mAdImageList.size() > 0) {
+            // 将图片装载到数组中,其中一组类似缓冲，防止图片少时出现黑色图片，即显示不出来
+            mImageViews[0] = new ImageView[mAdImageList.size()];
+            mImageViews[1] = new ImageView[mAdImageList.size()];
+            for (int i = 0; i < mImageViews.length; i++) {
+                for (int j = 0; j < mImageViews[i].length; j++) {
+                    iv = new ImageView(mContext);
+                    iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    Picasso.with(mContext).load(mAdImageList.get(j)).placeholder(R.mipmap.ic_launcher).into(iv);
+                    mImageViews[i][j] = iv;
+                }
+            }
+        } else {
+            mImageViews[0] = new ImageView[1];
+            mImageViews[1] = new ImageView[1];
+            for (int i = 0; i < mImageViews.length; i++) {
+                for (int j = 0; j < mImageViews[i].length; j++) {
+                    iv = new ImageView(mContext);
+                    iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    Picasso.with(mContext).load(R.mipmap.ic_launcher).into(iv);
+                    mImageViews[i][j] = iv;
+                }
+            }
+        }
+    }
+
+    OnBannerClickListener mOnBannerClickListener;
+
+    public interface OnBannerClickListener {
+        void onBannerClick(String action);
+    }
+
+}
+
+
+
